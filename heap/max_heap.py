c15442a7e23309721c5c88925c5088f50a3e6229
alasalle <angela.lasalle91@gmail.com>
2019-06-27 21:26:49 -0400

    insert on max_heap complete

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,25 +1,26 @@
 class Heap:
   def __init__(self):
-    self.storage = [0]
+    self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
     pass
 
   def get_max(self):
-    if self.storage[1]:
-      return self.storage[1]
+    if self.storage[0]:
+      return self.storage[0]
     else:
       return False
 
   def get_size(self):
-    return len(self.storage) - 1
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    parent = index//2
-    if index <= 1:
+    parent = (index - 1)//2
+    if index <= 0:
       return
     elif self.storage[index] > self.storage[parent]:
       self._swap(index, parent)
